3c3916897e3cc97411a39e70769efde36804c890
Howl222 <101348125+Howl222@users.noreply.github.com>
2022-06-21 11:27:36 +0200

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 function ctrlC(){
-	echo -e "\n\n[!]Saliendo...\n"
-	rm "extensions.txt" $tmpFile  &>/dev/null
-	exit 1
+	
+	echo "[!]Exit the program? (Default No)"
+	
+	read -r line
+	if [[ -n $line  ]]; then
+		echo -e "\n\n[!]Exit...\n"
+		exitFunc 1
+	fi
+	return 0 
+}
+
+function exitFunc(){
+	rm "$tmpFile" "$tmpFile2" &>/dev/null
+	tput cnorm
+	exit "$1"
 }
 
 trap ctrlC INT
-
+tput civis
 
 function help(){
 
@@ -16,51 +28,70 @@ function help(){
 	echo -e "\t\t-o \t\t\tOutput File"
 	echo -e "\t\t-w \t\t\tWordlist (Default: directory-list-2.3-medium)"
 	echo -e "\t\t-n \t\t\tDon't follow redirect"
-	echo -e "\t\t-s \t\t\tSubdomains (Default wordlist: subdomains-top1million-110000.txt)"
-	echo -e "\t\t-a \t\t\tHide words (Default: 0, Only with subdomain enumeration)"
-	echo -e "\t\t-c \t\t\tHide code (Default: 404)"
-	exit 0
+	echo -e "\t\t-c \t\t\tHide code "
+	echo -e "\t\t-hw \t\t\tHide Words"
+	echo -e "\t\t-l \t\t\tHttps mode"
+	echo -e "\t\t-s \t\t\tSubdomains enumeration(Default wordlist: subdomains-top1million-110000.txt)"
+	echo -e "\t\t-a \t\t\tAuto calibrate (Default: True) Only for subdomain enumeration"
+	echo -e "\t\t-z \t\t\tExtra options for Ffuf"
+	exitFunc
 }
 
 function createOutputFile(){
 
-	touch "$output"
-	echo -e "$url\n" >> "$output"
-	echo "$1" >> "$output"
+	touch "$output" 
+	cat "$1" >> "$output"
 	return 0
 }
 
-function writeOutputFile () {
-	local numTabs aux tabs dir newDirectories
-	numTabs=$(echo "$url" | grep -o '/' | wc -l)
+function writeOutputFile(){
+
+	local results resultsStatus numTabs aux tabs dir
+
+	dir="$2"
+	results=()  
+	resultsStatus=()
+
+	while IFS='' read -r line; do results+=("$line"); done < <(jq '.results[].input.FUZZ' "$tmpFile" | tr -d '"') 
+	while IFS='' read -r line; do resultsStatus+=("$line"); done < <(jq '.results[].status' "$tmpFile" | tr -d '"') 
+
 	aux=$(echo "$url" | awk -F "/" '{print $(NF)}')
-	tabs=$(for (( i=0; i < $numTabs - 1; i++ )); do echo -n -e "\t"; done)
-	dir=$tabs$aux
-	newDirectories=$(awk -F '\t' '{print $4}' "$tmpFile" | grep -oP '".*?"' | tr -d '"')
+	numTabs=$(echo "$url" | grep -o '/' | wc -l)
+
+	for (( i=0 ; i < $(echo "${results[@]}" | wc -w) ; i++ )) ; do
+		
+        if [[ ${results[$i]} == "" ]]; then
+            continue
+        fi
+        {
+		echo -n "$dir${results[$i]}" 
+		n=$(echo -n "$dir${results[$i]}" | wc -c)
+		for (( j=0; j < (80-n-numTabs*4); j++)); do
+		 	echo -n " "
+		done 
+		echo "${resultsStatus[$i]}" 
+		} >> "$tmpFile2"
+	done
+
+	if [[ ! -e "$tmpFile2" ]]; then
+		return 0
+	fi
+
+	if $1 ; then
+		echo -e "Subdomains: $url\n" > "$output"
+		cat "$tmpFile2" >> "$output"
+		return 0
+	fi
 
 	if [[ ! -e $output ]]; then
-		createOutputFile "$newDirectories"
-		return 0 
+	    createOutputFile "$tmpFile2"
+	  	return 0  
+    else
+		cat "$tmpFile2" >> "$output"
+		sort -u "$output" | sponge "$output"
 	fi
 
-	if [[ "$dir" == "" ]]; then
-		 echo "$newDirectories" >> "$output"
-		 return 0
-	fi
-	while read -r line; do
-		if [[ "$line" == "$dir" ]]; then
-			echo -e "\n$tabs\t"
-			echo "$newDirectories"
-		else
-			echo "$line" 
-		fi
-	done < "$output" >> "tmpFile.txt"
-		
-	rm "$output" &>/dev/null
-
-	mv "tmpFile.txt" "$output"
-
-	return 0 
+	return 0
 }
 
 
@@ -69,60 +100,77 @@ if [[ $# -eq 0  ]]; then
 fi
 
 url=""
-output="webScan.txt"
+output=""
 wordlist="/usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt"
-hideWords="0"
-hideCode="404"
+hideCode=""
+hideWords=""
 subdomain=false
-redirect="-L"
-tmpFile="tmpFile.txt"
-
-while getopts ":e:o:a:w:c:" arg
-do
-    case $arg in
-        e) extensions=${OPTARG};;
-		o) output=${OPTARG};;
-		w) wordlist=${OPTARG};;
-		a) hideWords=${OPTARG};;
-		c) hideCode=${OPTARG};;
-    esac
-done
-
-
+redirect="-r"
+autoCalibrate="-ac"
+extraOptions=""
+tmpFile="tmpFile.txt" #/tmp/tmpFile.txt
+tmpFile2="tmpFile2.txt" # New directories and files discovered
+req="http"
 
 while [[ -n $1 ]]; do
 	
-	if [[ "$1" == "-s"  ]]; then
-		subdomain=true
-	elif [[ "$1" == "-h" || "$1" == "--help" ]]; then
+	if [[ "$1" == "-h" || "$1" == "--help"  ]]; then
 		help
+	elif [[ "$1" == "-s" ]]; then
+		subdomain=true
+	elif [[ "$1" == "-e" ]]; then
+		extensions="$2"
+	elif [[ "$1" == "-o" ]]; then
+		output="$2"
+	elif [[ "$1" == "-hw" ]]; then
+		hideWords="-fw $2"
+	elif [[ "$1" == "-w" ]]; then
+		wordlist="$2"
+	elif [[ "$1" == "-c" ]]; then
+		hideCode="-fc $2"
 	elif [[ "$1" == "-n" ]]; then
 		redirect=""
+	elif [[ "$1" == "-a" ]]; then
+		autoCalibrate=""
+	elif [[ "$1" == "-l" ]]; then
+		req="https"
+	elif [[ "$1" == "-z" ]]; then
+		extraOptions="$2"
 	fi
 
 	url="$1"
 	shift
 done
 
+if [[ -z "$output" ]]; then
+	output="$url""WebScan.txt"
+fi
+
 
 url=$(echo "$url" | sed 's/\/\//\//g')
-#url="${url[@]:0:${#url[@]}-1}"
-
-defaultChars=$(curl -s "http://$url/asdkjjnfdjnasfdj" | wc -c)
 
 if $subdomain ; then
 	if [[ $wordlist == "/usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt" ]]; then
-		wordlist="/usr/share/secLists/Discovery/DNS/subdomains-top1million-110000.txt"
+		wordlist="/usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt"
 	fi
-	wfuzz -c -t 200 "--hw=$hideWords" -L -f "subdomainScan.txt" -w  "$wordlist" -H "Host:FUZZ.$url"  "http://$url/" 2> /dev/null
+	ffuf -c -t 200 -o $tmpFile $hideWords $hideCode $extraOptions $autoCalibrate -w $wordlist -u "$req://$url/" -H "Host: FUZZ.$url"
+	error=$?
+	output="subdomainEnumeration.txt"
+	writeOutputFile true
 elif [[ -z "$extensions" ]]; then
-	wfuzz -c -t 200 "--hc=$hideCode"  "--hh=$defaultChars" "$redirect"  -f "$tmpFile" -w  "$wordlist" "http://$url/FUZZ" 2> /dev/null
-	writeOutputFile
+	ffuf -c -t 200 -o $tmpFile  -w "$wordlist" $redirect $hideWords $hideCode $extraOptions -u "$req://$url/FUZZ"
+	error=$?
+	writeOutputFile false "$req://$url/"
 else
-	echo "$extensions" |  tr "," '\n' > "extensions.txt"
-	wfuzz -c -t 200 "--hc=$hideCode" "--hh=$defaultChars" "$redirect"  -f "$tmpFile" -w  "$wordlist" -w "extensions.txt" "http://$url/FUZZ.FUZ2Z" 2> /dev/null
-	rm "extensions.txt" &>/dev/null
+	extensions=$( echo "$extensions" | tr ',' '\n' | ts '.' | tr -d ' ' | tr '\n' ',' )
+	unset extensions[${#extensions[@]}]
+	ffuf -c -t 200 -o $tmpFile  -w "$wordlist" $redirect -e "$extensions" $hideWords $hideCode $extraOptions -u "$req://$url/FUZZ"
+	error=$?
+	writeOutputFile false
 fi
 
-rm $tmpFile &>/dev/null
+if [[ "$error" != "0" ]]; then
+	echo -e "\n\n[-]An error has occurred"
+fi
 
+exitFunc 0 
